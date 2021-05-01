8cd8ae4fd096b08a3dcc0ce7e7e6568897e3757f
Jake Peterson <jpeterson0514@gmail.com>
2021-05-01 05:16:34 +0000

    Automatically backed up by Learn

---
+++ konami_code.js
@@ -0,0 +1,17 @@
+
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+function init() {
+  // your code here
+  function keyHandler(e) {
+    const key = parseInt(e.detail || e.which);
+    if (code.indexOf(key) !== -1) {
+      alert("YOU DID IT");
+    } else {
+      return e.preventDefault();
+    }
+  }
+  document.body.addEventListener('keydown', keyHandler);
+}
+
+init();

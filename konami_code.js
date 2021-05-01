e02530433d1e44d0b91998355d5e61d490a15eab
Jake Peterson <jpeterson0514@gmail.com>
2021-05-01 05:18:18 +0000

    Automatically backed up by Learn

---
+++ konami_code.js
@@ -0,0 +1,32 @@
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+
+let index = 0;
+
+function init() {
+  // your code here
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+    
+    if (key === codes[index]) {
+      index++;
+      
+      if (index === codes.length) {
+        alert("You cracked the code!");
+        
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
